bbc7cc16949fbf3d21a525616ef90db7e0f17435
Tancsics Gergely <43749897+TancsicsGergely@users.noreply.github.com>
2019-05-26 18:25:41 +0200

    Create queue.py

---
+++ Bot Files/Test in progress/queue.py
@@ -0,0 +1,76 @@
+songs = asyncio.Queue()
+play_next_song = asyncio.Event()
+
+
+async def audio_player_task():
+    while True:
+        play_next_song.clear()
+        current = await songs.get()
+        current.start()
+        await play_next_song.wait()
+
+
+def toggle_next():
+    client.loop.call_soon_threadsafe(play_next_song.set)
+
+
+@client.command(pass_context=True)
+async def testplay(ctx, * ,url, ytdl_options=None, **kwarg):
+        if not ctx.message.author.bot:
+            server = ctx.message.server
+            voice_client = client.voice_client_in(server)
+            if voice_client == None:
+                await client.say("Please wait. :musical_note:")
+                try:
+                    channel = ctx.message.author.voice.voice_channel
+                    await client.join_voice_channel(channel)
+                except:
+                    return False
+                try:
+                    server = ctx.message.server
+                    voice_client = client.voice_client_in(server)
+                    ydl_opts = {
+                        'format': 'bestaudio/best',
+                        'postprocessors': [{
+                            'key': 'FFmpegExtractAudio',
+                            'preferredcodec': 'mp4',
+                            'preferredquality': '192',
+                        }],
+                    }
+                    player = await voice_client.create_ytdl_player("ytsearch: {}".format(url), before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5", after=toggle_next)
+                    player.start()
+                    await songs.put(player)
+                    global playing
+                    await client.say("Starting the music...\nTitle: **{}**".format(player.title))
+                    global players
+                    players[server.id] = player 
+                    player.volume = 0.2
+                    global playing 
+                    playing = "Now playing: **{}**".format(player.title)
+                    global last_played
+                    last_played = player.url
+#                    embed = discord.Embed(title="Music Player", description="Informations: ", color=0x00ff00)
+#                    embed.add_field(name="Song title:",value=player.title, inline=True)
+#                    embed.add_field(name="Date of uploading:",value=player.upload_date, inline=True)
+#                    embed.add_field(name="Uploader:",value=player.uploader, inline=True)
+#                    embed.add_field(name="Requested by:",value=ctx.message.author,inline=True)
+#                    embed.set_author(name=ctx.message.author.name, icon_url=ctx.message.author.icon_url)
+#                    await client.say(embed=embed)
+#                    await client.say(embed=embed)
+                except:
+                    await client.say("Oops! Something went wrong. Please try ``!VTU leave`` and try again. :x:")
+                while not player.is_done():
+                    await asyncio.sleep(1)
+                    player = await voice.create_ytdl_player(url, after=toggle_next)
+                    await songs.put(player) 
+                try:
+                    server = ctx.message.server
+                    voice_client = client.voice_client_in(server)
+                    await voice_client.disconnect()
+                    await client.say("The song is over. I left the voice channel. :white_check_mark: ")
+                except:
+                    return False
+            else:
+                await client.say("Something is not good! Use ``!VTU leave``!")
+        else:
+            return False
